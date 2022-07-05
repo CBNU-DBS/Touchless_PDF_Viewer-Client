3d7f3917112fc3fe7c9aa232e06c319f99865893
ckwogus37 <ckwogus37@gmail.com>
2022-07-05 16:36:59 +0900

    개인정보 변경화면 구현

---
+++ Client/app/src/main/java/com/example/client/Personal_info_setting_Activity.java
@@ -0,0 +1,51 @@
+package com.example.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Personal_info_setting_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info_setting);
+
+        Button password_set_Btn = (Button) findViewById(R.id.password_set_btn);
+        password_set_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //비밀번호를 변경합니다.
+                //DB와 연동하여 비밀번호가 변경되면 토스트 메세지를 띄웁니다.
+            }
+        });
+
+        Button email_set_Btn = (Button) findViewById(R.id.email_set_btn);
+        email_set_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //이메일을 변경합니다.
+                //DB와 연동하여 이메일이 변경되면 토스트 메세지를 띄웁니다.
+            }
+        });
+
+        Button phonenum_set_Btn = (Button) findViewById(R.id.phonenum_set_btn);
+        phonenum_set_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //전화번호를 변경합니다.
+                //DB와 연동하여 전화번호가 변경되면 토스트메세지를 띄웁니다.
+            }
+        });
+
+        Button back_to_mypage_Btn = (Button) findViewById(R.id.back_to_mypage_btn);
+        phonenum_set_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //마이페이지로 돌아갑니다.
+            }
+        });
+    }
+}
