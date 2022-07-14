424301a71b97716b05ef1012ed307ffa26595ffc
seogi98 <seogi9898@gmail.com>
2022-07-14 22:37:25 +0900

    bugfix pdfview 수정

--- Client/app/src/main/java/com/example/client/fragment/DocumentFragment.java
+++ Client/app/src/main/java/com/example/client/fragment/DocumentFragment.java
@@ -1,21 +1,45 @@
 package com.example.client.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.client.Adapter.PdfAdapter;
 import com.example.client.R;
+
+import java.util.ArrayList;
 
 public class DocumentFragment extends Fragment {
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // 리사이클러뷰에 표시할 데이터 리스트 생성.
+        super.onCreate(savedInstanceState);
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(String.format("Title %d", i));
+        }
+        RecyclerView recyclerView = getView().findViewById(R.id.PdfRecycler);
+        Log.e("recyclerView",recyclerView+"");
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        PdfAdapter adapter = new PdfAdapter(list);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_select_pdf, container, false);
     }
