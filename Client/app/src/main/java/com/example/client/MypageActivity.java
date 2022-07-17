3cc8b03bb5b541c811c45061f3a26f7ac054cce2
ckwogus37 <ckwogus37@gmail.com>
2022-07-17 23:26:34 +0900

    마이페이지 화면 오류 해결 및 구글 드라이브 기능 구현

--- Client/app/src/main/java/com/example/client/MypageActivity.java
+++ Client/app/src/main/java/com/example/client/MypageActivity.java
@@ -17,6 +17,17 @@ public class MypageActivity extends AppCompatActivity {
         // user_name, user_mail, user_phone, user_google_drive_info shoud contain personal info and
         // another user login, then all info have to be changed
 
+        // 구글 드라이브 버튼을 누르면 스마트폰에 다운로드 되어있는 "구글 드라이브" 앱이 실행됩니다.
+        String googledrive = "com.google.android.apps.docs";
+        Intent intentgoogledrive = getPackageManager().getLaunchIntentForPackage(googledrive);
+
+        Button button = findViewById(R.id.btn_google_drive);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MypageActivity.this.startActivity(intentgoogledrive);
+            }
+        });
 
     }
 }
