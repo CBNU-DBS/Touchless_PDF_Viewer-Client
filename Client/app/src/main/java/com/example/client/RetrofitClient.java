78d5d23ea8516f6524195a415f3fc198b21aab7b
Kang Min <rkdals133@gmail.com>
2022-10-06 22:12:10 +0900

    서버 ip 설정

--- Client/app/src/main/java/com/example/client/RetrofitClient.java
+++ Client/app/src/main/java/com/example/client/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private final static String BASE_URL = "http://10.0.2.2:8080";
+    private final static String BASE_URL = "http://52.79.244.120:8080";
     private static Retrofit retrofit = null;
 
     private RetrofitClient(){
